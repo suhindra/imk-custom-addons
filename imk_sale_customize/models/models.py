1b59c747411e5977dde701fc2edb14a904e8626d
Suhindra <suhindra@hotmail.co.id>
2022-03-14 18:37:58 +0700

    Update models.py

--- imk_sale_customize/models/models.py
+++ imk_sale_customize/models/models.py
@@ -16,6 +16,6 @@ class SaleOrderLine(models.Model):
 
     @api.onchange('product_id')
     def product_id_change(self):
-        res = super(SaleOrder, self).product_id_change()
+        res = super(SaleOrderLine, self).product_id_change()
         if self.product_id.product_template_attribute_value_ids:
             self.product_template_attribute_value_ids = [(6, 0, self.product_id.attribute_line_ids.ids)]
