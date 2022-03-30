d8dd2f7fb9f19780ace3b0887248896d13e86413
Suhindra <suhindra@hotmail.co.id>
2022-03-30 10:46:07 +0700

    Update volume.py

--- product_volume/models/volume.py
+++ product_volume/models/volume.py
@@ -32,23 +32,20 @@ class ProductDimensionsVolume(models.Model):
 
 
     @api.onchange('length','breadth','height','kw1','kw2','kw3','kw4','kw5','flute') 
-    def onchange_l_b_h_kw(self):
-        if not self.flute:
-            length_sheet = 0;
-            breadth_sheet = 0;
-            if self.flute == 'bf':
-                length_sheet = 2 * (int(self.length if self.length else 0) + int(self.breadth if self.breadth else 0)) + 12 + 10 + 35 
-                breadth_sheet = int(self.breadth if self.breadth else 0) + int(self.height if self.height else 0) + 9
-                
-            if self.flute == 'cf':
-                length_sheet = 2 * (int(self.length if self.length else 0) + int(self.breadth if self.breadth else 0)) + 15 + 10 + 35 
-                breadth_sheet = int(self.breadth if self.breadth else 0) + int(self.height if self.height else 0) + 13
-                
-            if self.flute == 'bcf':
-                length_sheet = 2 * (int(self.length if self.length else 0) + int(self.breadth if self.breadth else 0)) + 28 + 10 + 40 
-                breadth_sheet = int(self.breadth if self.breadth else 0) + int(self.height if self.height else 0) + 22
-                
-            sheet_size = length_sheet * breadth_sheet / 1000000
-            self.wide = sheet_size
-            weight = sheet_size * (int(re.sub('\D', '',self.kw1 if self.kw1 else 0)) + (int(re.sub('\D', '',self.kw2 if self.kw2 else 0)) * 1.4) + int(re.sub('\D', '',self.kw3 if self.kw3 else 0)) + (int(re.sub('\D', '',self.kw4 if self.kw4 else 0)) * 1.5) + int(re.sub('\D', '',self.kw5 if self.kw5 else 0))) / 1000
-            self.weight = weight        
+    def onchange_l_b_h_kw(self):    
+        if self.flute == 'bf':
+            length_sheet = 2 * (int(self.length if self.length else 0) + int(self.breadth if self.breadth else 0)) + 12 + 10 + 35 
+            breadth_sheet = int(self.breadth if self.breadth else 0) + int(self.height if self.height else 0) + 9
+            
+        if self.flute == 'cf':
+            length_sheet = 2 * (int(self.length if self.length else 0) + int(self.breadth if self.breadth else 0)) + 15 + 10 + 35 
+            breadth_sheet = int(self.breadth if self.breadth else 0) + int(self.height if self.height else 0) + 13
+            
+        if self.flute == 'bcf':
+            length_sheet = 2 * (int(self.length if self.length else 0) + int(self.breadth if self.breadth else 0)) + 28 + 10 + 40 
+            breadth_sheet = int(self.breadth if self.breadth else 0) + int(self.height if self.height else 0) + 22
+            
+        sheet_size = length_sheet * breadth_sheet / 1000000
+        self.wide = sheet_size
+        weight = sheet_size * (int(re.sub('\D', '',self.kw1 if self.kw1 else 0)) + (int(re.sub('\D', '',self.kw2 if self.kw2 else 0)) * 1.4) + int(re.sub('\D', '',self.kw3 if self.kw3 else 0)) + (int(re.sub('\D', '',self.kw4 if self.kw4 else 0)) * 1.5) + int(re.sub('\D', '',self.kw5 if self.kw5 else 0))) / 1000
+        self.weight = weight        
