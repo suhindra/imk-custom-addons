f3d7f2297ea366769b6619b2096dacb1ae27c226
Suhindra <suhindra@hotmail.co.id>
2022-04-19 15:07:38 +0700

    ref

--- product_supplierinfo_for_customer/models/product_template.py
+++ product_supplierinfo_for_customer/models/product_template.py
@@ -9,8 +9,6 @@ from odoo import fields, models
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    ref = fields.Char(string='Product Ref')
-
     customer_ids = fields.One2many(
         comodel_name="product.customerinfo",
         inverse_name='product_tmpl_id',
